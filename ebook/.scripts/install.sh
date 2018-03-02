041dbe80df6285a0883bbf5abab780363dd3da77
Warren R Bank <warren.r.bank@gmail.com>
2018-03-01 21:35:58 -0800

    add Windows platform detection to Calibre bash install script

--- ebook/.scripts/install.sh
+++ ebook/.scripts/install.sh
@@ -3,8 +3,16 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$DIR/env.sh"
 
-if [ ! -d "$DIR_EBOOK/Calibre Portable" ]; then
-  installer="$DIR_EBOOK/calibre-portable-installer.exe"
-  wget -nc --no-check-certificate -O "$installer" "https://download.calibre-ebook.com/3.18.0/calibre-portable-installer-3.18.0.exe"
-  "$installer" "$DIR_EBOOK"
+# under Windows, download and install a PortableApps version of Calibre
+if [ "$OSTYPE" == "msys" -o "$OSTYPE" == "cygwin" ]; then
+  # check whether the PortableApps directory already exists
+  if [ ! -d "$DIR_EBOOK/Calibre Portable" ]; then
+    installer="$DIR_EBOOK/calibre-portable-installer.exe"
+    wget -nc --no-check-certificate -O "$installer" "https://download.calibre-ebook.com/3.18.0/calibre-portable-installer-3.18.0.exe"
+    "$installer" "$DIR_EBOOK"
+  fi
 fi
+
+# For other platforms, Calibre is an external dependency.
+# Please confirm that its "ebook-convert" binary can be found in $PATH:
+#   which "ebook-convert"
