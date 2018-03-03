5bbef0c6bf652bc11c759981adb9a1b728e2ec47
Warren R Bank <warren.r.bank@gmail.com>
2018-03-03 05:16:53 -0800

    new methodology: copy "sources" into root folder instead of linking
    * my changes to gitbook makes the linking methodology work very nicely
      * https://github.com/warren-bank/gitbook/commit/76e4bc88f8d6e43446eb8289cc592154e6edf44f
      * https://github.com/GitbookIO/gitbook/pull/1992
    * however
      * this PR probably won't get merged
      * the "sources" directory would still be written to a non-temporary area of the file system
    * the simplest solution is probably the better one
      * copy the "sources" directory into the gitbook root folder
        * this is the structure gitbook expects
        * the directory is copied into the temporary working directory
          * it's removed after the build process is finished
      * exclude this copy from the repo
      * the "gitbook:clean" run-script removes all source files from the gitbook root folder
        * in most cases, it should be run before a new build
        * the "gitbook:all" run-script calls it before building: pdf,ebook,mobi formats

--- ebook/.scripts/copy_assets.sh
+++ ebook/.scripts/copy_assets.sh
@@ -4,3 +4,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$DIR/env.sh"
 
 [ -d "$DIR_EBOOK_SRC/assets" ] || cp --recursive "$DIR_GUIDE/assets" "$DIR_EBOOK_SRC/assets"
+[ -d "$DIR_EBOOK_SRC/source" ] || cp --recursive "$DIR_GUIDE/source" "$DIR_EBOOK_SRC/source"
--- ebook/.scripts/parser.js
+++ ebook/.scripts/parser.js
@@ -1,11 +1,8 @@
 const fs = require('fs')
 
-const modify_relative_links = markdown => markdown.replace(/(\]\()(?:\.?\/)?(source\/)/g, '$1../../$2')
-
 const README   = {}
 README.path    = __dirname + '/../../README.md'
 README.content = fs.readFileSync(README.path, {encoding: "utf8"})
-README.content = modify_relative_links(README.content)
 
 const pages    = {}
 pages.README   = __dirname + '/../src/book_README.md'
